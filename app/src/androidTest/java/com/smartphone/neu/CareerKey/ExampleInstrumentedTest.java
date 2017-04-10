693cc1fc4675ff62eb9653be70cec84346bbfb44
AndersonHJ <houjian.max@gmail.com>
2017-04-10 15:28:31 -0700

    configuration update

--- app/src/androidTest/java/com/smartphone/neu/CareerKey/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/smartphone/neu/CareerKey/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.luqifei.homework", appContext.getPackageName());
+        assertEquals("com.smartphone.neu.CareerKey", appContext.getPackageName());
     }
 }
