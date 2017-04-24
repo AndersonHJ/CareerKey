98f54e27c9bb7563bcf4f2497d4d8320d22560b3
Ying Liao <liaoying.shufe@gmail.com>
2017-04-23 17:50:08 -0700

    Add toolbar to Event detail

--- app/src/main/java/com/smartphone/neu/CareerKey/EventDetailActivity.java
+++ app/src/main/java/com/smartphone/neu/CareerKey/EventDetailActivity.java
@@ -26,14 +26,6 @@ public class EventDetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         startTime = (TextView) findViewById(R.id.startTime);
