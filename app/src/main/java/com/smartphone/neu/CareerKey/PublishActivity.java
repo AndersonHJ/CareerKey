060f32f206038c1e057102e68e3e9c922e47172d
AndersonHJ <houjian.max@gmail.com>
2017-04-24 11:45:44 -0700

    updates 7.3

--- app/src/main/java/com/smartphone/neu/CareerKey/PublishActivity.java
+++ app/src/main/java/com/smartphone/neu/CareerKey/PublishActivity.java
@@ -3,25 +3,49 @@ package com.smartphone.neu.CareerKey;
 import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class PublishActivity extends AppCompatActivity {
 
     private EditText date;
     private DatePickerDialog datePickerDialog;
     private Button submit;
+    private EditText time;
+    private EditText title;
+    private EditText duration;
+    private EditText description;
+    private EditText address;
+    private EditText city;
+    private EditText state;
+    private RadioButton course, event;
+    private RadioGroup group;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publish);
 
+        title = (EditText) findViewById(R.id.txtTitle);
+        time = (EditText) findViewById(R.id.txtStartTime);
+        duration = (EditText) findViewById(R.id.txtDuration);
+        description = (EditText) findViewById(R.id.txtDescription);
+        address = (EditText) findViewById(R.id.txtAddress);
+        city = (EditText) findViewById(R.id.txtCity);
+        state = (EditText) findViewById(R.id.txtState);
+        group = (RadioGroup) findViewById(R.id.activityType);
+        course = (RadioButton) findViewById(R.id.courseRB);
+        event = (RadioButton) findViewById(R.id.eventRB);
 
         date = (EditText) findViewById(R.id.txtDatePicker);
         date.setOnClickListener(new View.OnClickListener() {
@@ -53,9 +77,27 @@ public class PublishActivity extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String[] datetime = date.getText().toString().split("/");
+                int year = Integer.parseInt(datetime[2]);
+                int month = Integer.parseInt(datetime[1]);
+                int day = Integer.parseInt(datetime[0]);
+                String[] hourandminute = time.getText().toString().split(":");
+                int hour = Integer.parseInt(hourandminute[0]);
+                int minute = Integer.parseInt(hourandminute[1]);
+                int selectId = group.getCheckedRadioButtonId();
+                int type = 0;
+                if(selectId == course.getId()){
+                    type = 2;
+                }
+                else{
+                    type = 1;
+                }
+
                 DataManager dataManager = new DataManager();
-                dataManager.addActivity(null, "");
-                finish();
+//                dataManager.addActivity(new EventItem(title.getText().toString(), new Date(year, month, day, hour, minute),
+//                        Integer.parseInt(duration.getText().toString()), description.getText().toString(),
+//                        type, address.getText().toString(), city.getText().toString(), state.getText().toString(), ), "");
+                //finish();
             }
         });
 
